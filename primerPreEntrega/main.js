a95ed3925aa58fd613e99f80fc55c0a31f66791b
fercarballo88 <fercarballo@live.com>
2023-05-03 13:32:22 -0300

    mod 1 primer preEntrega

--- primerPreEntrega/main.js
+++ primerPreEntrega/main.js
@@ -38,41 +38,49 @@ do{
 
 
 
-    let aviso = parseInt(prompt("Ingrese el Numero de Set:   1_X-Wing     2_Imperial Shuttle      3_Naboo Starfighter    4_Razor Crest     5_Tie Fighter     6_Millenium Falcon     7_Imperial Cruiser   INGRESE esc PARA CANCELAR"));
+    let aviso = parseInt(prompt("Ingrese el Numero de Set:   1_X-Wing     2_Imperial Shuttle      3_Naboo Starfighter    4_Razor Crest     5_Tie Fighter     6_Millenium Falcon     7_Imperial Cruiser   INGRESE 0 PARA CANCELAR"));
     const naves = ["No Ingresó un número valido", "X-wing", "Imperial Shuttle", " Naboo Starfighter", "Razor Crest", "Tie Fighter", "Millenium Falcon", "Imperial Cruiser"];
 
-while(aviso !="esc"){  
+
+
+
+while(aviso !=0){  
 
             let cantidad =parseInt(prompt("Ingrese cantidad de Sets"));
-            let envio = 2000;
+            const envio = 2000;
+            
+            function calculo(x){
+                const resultado = (x*400)*cantidad+envio
+                return resultado
+                }
 
 
         
         if(aviso==1){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 50" + " Cantidad: " + cantidad + " En Pesos mas envio: " + (((50*400)*cantidad)+envio)
+        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 50" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(50)
         );
         }else if(aviso==2){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 60" + " Cantidad: " + cantidad + " En Pesos mas envio: " + (((60*400)*cantidad)+envio)
+        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 60" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(60)
         );
         }else if(aviso==3){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 70" + " Cantidad: " + cantidad + " En Pesos mas envio: " + (((70*400)*cantidad)+envio)
+        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 70" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(70)
         );
         }else if(aviso==4){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 80" + " Cantidad: " + cantidad + " En Pesos mas envio: " + (((80*400)*cantidad)+envio)
+        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 80" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(80)
         );
         }else if(aviso==5){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 40" + " Cantidad: " + cantidad + " En Pesos mas envio: " + (((40*400)*cantidad)+envio)
+        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 40" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(40)
         );
         }else if(aviso==6){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 100" + " Cantidad: " + cantidad + " En Pesos mas envio: " + (((100*400)*cantidad)+envio)
+        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 100" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(100)
         );
         }else if(aviso==7){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 200" + " Cantidad: " + cantidad +  " En Pesos mas envio: " + (((200*400)*cantidad)+envio)
+        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 200" + " Cantidad: " + cantidad +  " En Pesos mas envio: " + calculo(200)
         );
     }else{
         alert(naves[0])
     };
 
-    aviso = parseInt(prompt("Ingrese el Numero de Set:   1_X-Wing     2_Imperial Shuttle      3_Naboo Starfighter    4_Razor Crest     5_Tie Fighter     6_Millenium Falcon     7_Imperial Cruiser"))
+    aviso = parseInt(prompt("Ingrese el Numero de Set:   1_X-Wing     2_Imperial Shuttle      3_Naboo Starfighter    4_Razor Crest     5_Tie Fighter     6_Millenium Falcon     7_Imperial Cruiser   INGRESE 0 PARA CANCELAR"));
 }
 
