0fd2bc2e3afa5cb6ff88c560bdb2c6141c5589d0
fercarballo88 <fercarballo@live.com>
2023-05-18 20:15:27 -0300

    mod 2 segundapreEntrega

--- primerPreEntrega/main.js
+++ primerPreEntrega/main.js
@@ -1,46 +1,50 @@
-    let aviso = parseInt(prompt("Ingrese el Numero de Set:\n1_X-Wing\n2_Imperial Shuttle\n3_Naboo Starfighter\n4_Razor Crest\n5_Tie Fighter\n6_Millenium Falcon\n7_Imperial Cruiser\nINGRESE 0 PARA CANCELAR"));
-    const naves = ["No Ingresó un número valido", "X-wing", "Imperial Shuttle", " Naboo Starfighter", "Razor Crest", "Tie Fighter", "Millenium Falcon", "Imperial Cruiser"];
-
-
-
-
-while(aviso !=0){  
-
-            let cantidad =parseInt(prompt("Ingrese cantidad de Sets"));
-            const envio = 2000;
-
-            function calculo(x){
-                const resultado = (x*400)*cantidad+envio
-                return resultado
-                }
-
-
-        
-        if(aviso==1){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 50" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(50)
-        );
-        }else if(aviso==2){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 60" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(60)
-        );
-        }else if(aviso==3){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 70" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(70)
-        );
-        }else if(aviso==4){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 80" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(80)
-        );
-        }else if(aviso==5){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 40" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(40)
-        );
-        }else if(aviso==6){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 100" + " Cantidad: " + cantidad + " En Pesos mas envio: " + calculo(100)
-        );
-        }else if(aviso==7){
-        alert("Nombre del Set: " + naves[aviso] + " Precio en dolares: 200" + " Cantidad: " + cantidad +  " En Pesos mas envio: " + calculo(200)
-        );
-    }else{
-        alert(naves[0])
-    };
-
-    aviso = parseInt(prompt("Ingrese el Numero de Set:\n1_X-Wing\n2_Imperial Shuttle\n3_Naboo Starfighter\n4_Razor Crest\n5_Tie Fighter\n6_Millenium Falcon\n7_Imperial Cruiser\nINGRESE 0 PARA CANCELAR"));
+class Producto{
+    constructor(nombre,precio,numero,cantidadPiezas){
+        this.nombre = nombre
+        this.precio = precio
+        this.numero = numero
+        this.cantidadPiezas = cantidadPiezas
+    }
 }
+
+const naves = []
+
+let continuar = true
+
+while(continuar===true){
+    const nombre = prompt("Ingrese Nombre del Set Lego").toUpperCase()
+    const precio = prompt("Ingrese Precio en Dolares")
+    const numero = prompt("Ingrese Numero del Set")
+    const cantidadPiezas = prompt("Cuantas Piezas Tiene")
+    const nuevoSet = new Producto(nombre,precio,numero,cantidadPiezas)
+    naves.push(nuevoSet)
+    continuar = confirm("Desea continuar con la carga?")
+};
+
+let verLista = prompt("Desea ver la lista creada?\nsi\nno")
+if(verLista === "si"){
+for(let i = 0; i < naves.length; i++){
+    console.log(naves[i])}
+};
+
+const eliminarElemento = prompt("Desea Eliminar Set de la lista\nsi\nno")
+if(eliminarElemento == "si"){
+    const elementoAEliminar = prompt("Ingrese Numero del Set a Eliminar")
+    const index = naves.findIndex((nuevoSet)=> nuevoSet.numero === elementoAEliminar)
+    if(index != -1){
+        naves.splice(index,1)
+        alert("Producto Eliminado")
+        for(let i = 0; i < naves.length; i++){
+            console.log(naves[i])}
+}
+};
+
+const calculoPesos = prompt("Desea calcular el precio en Pesos + Envio\nsi\nno")
+if(calculoPesos=="si"){
+    const envio = 2000
+    const calcular = parseFloat(prompt("Ingrese el Precio en dolares"))
+    alert("precio en Pesos mas Envio: "+((calcular*400)+envio))
+};
+
+
 
