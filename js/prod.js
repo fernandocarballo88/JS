6e15f705ef874a14dec70bca6b43c84ffc6ee99d
fercarballo88 <fercarballo@live.com>
2023-06-03 09:48:47 -0300

    mod 8 tercerpreEntrega

--- js/prod.js
+++ js/prod.js
@@ -1,31 +1,38 @@
 const productos = [
     {
         nombre:"X wing",
+        img: src="../assets/img/xwing.jpg",
         precio:50,
         stock:4,
     },
     {
         nombre:"Imperial Shuttle",
+        img: src="../assets/img/imperial shuttle.jpeg",
         precio:60,
         stock:3,
     },
     {
         nombre:"Naboo Starfighter",
+        img: src="../assets/img/naboo n1.jpg",
+
         precio:80,
         stock:1,
     },
     {
         nombre:"Razor Crest",
+        img: src="../assets/img/razor crest.jpg",
         precio:100,
         stock:2,
     },
     {
         nombre:"Tie Fighter",
+        img: src="../assets/img/tie fighter.jpg",
         precio:30,
         stock:10,
     },
     {
         nombre:"Imperial Cruisser",
+        img: src="../assets/img/imperial cruiser.jpg",
         precio:200,
         stock:1,
     },
@@ -37,7 +44,7 @@ productos.forEach((producto)=>{
     tarjetaProducto.className = "tarjetaProducto"
     tarjetaProducto.innerHTML = `
                                 <h3>${producto.nombre}</h3>
-                                <img src="../assets/lego.jpg">
+                                <img src="${producto.img}">
                                 <span>Prcio: ${producto.precio}$</span>
                                 <span>Stock: ${producto.stock}</span>
                                 <button>COMPRAR</button>
