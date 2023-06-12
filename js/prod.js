55f3c459722cbae01ef2e808aa1518a4a5b85535
fercarballo88 <fercarballo@live.com>
2023-06-12 16:22:07 -0300

    mod 1 entergaFinal

--- js/prod.js
+++ js/prod.js
@@ -1,36 +1,41 @@
 const productos = [
     {
+        id: 1,
         nombre:"X wing",
         img: src="../assets/img/xwing.jpg",
         precio:50,
         stock:4,
     },
     {
+        id: 2,
         nombre:"Imperial Shuttle",
         img: src="../assets/img/imperial shuttle.jpeg",
         precio:60,
         stock:3,
     },
     {
+        id: 3,
         nombre:"Naboo Starfighter",
         img: src="../assets/img/naboo n1.jpg",
-
         precio:80,
         stock:1,
     },
     {
+        id: 4,
         nombre:"Razor Crest",
         img: src="../assets/img/razor crest.jpg",
         precio:100,
         stock:2,
     },
     {
+        id: 5,
         nombre:"Tie Fighter",
         img: src="../assets/img/tie fighter.jpg",
         precio:30,
         stock:10,
     },
     {
+        id: 6,
         nombre:"Imperial Cruisser",
         img: src="../assets/img/imperial cruiser.jpg",
         precio:200,
@@ -47,6 +52,8 @@ productos.forEach((producto)=>{
                                 <img src="${producto.img}">
                                 <span>Prcio: ${producto.precio}$</span>
                                 <span>Stock: ${producto.stock}</span>
+                                <form>
+                                </form>
                                 <button>COMPRAR</button>
 `
 contenedorProductos.append(tarjetaProducto)
